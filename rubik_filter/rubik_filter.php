1f6d7dff8669ad35d903fd747739050675c553e8
Tomas Spanel <tomas.spanel@gmail.com>
2019-11-13 17:39:53 +0100

    added xDebug module

--- rubik_filter/rubik_filter.php
+++ rubik_filter/rubik_filter.php
@@ -10,21 +10,35 @@
  */
 class rubik_filter extends rcube_plugin
 {
-    public $task = 'settings';
+    private $rc;
 
-    function init() { 
+    function init() {
+        $this->rc = rcube::get_instance();
+        
         // localization
         $this->add_texts('localization/', true);
-        
-        $this->add_hook('settings_action', array($this, 'settings_hook'));
+
+        $this->add_hook('folders_list', array($this, 'test'));
+        $this->add_hook('settings_actions', array($this, 'settings_hook'));
+
+        $this->rc->console("Hello console\n");
+        $this->rc->console("Hello console 2\n");
+    }
+
+    function test($args) {
+        $this->rc->console("test hook\n");
     }
 
     function settings_hook($args) {
+        $rc = rcube::get_instance();
+        $rc->console("In the hook\n");
+        $rc->console($args);
+
         $section = array(
-            'command' => '',
+            'command' => 'test',
             'type' => 'link',
             'label' => 'rubik_filter.settings_title',
-            'class' => ''
+            'class' => 'filter'
         );
 
         $args['actions'][] = $section;
